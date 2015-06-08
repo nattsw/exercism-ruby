16d4c55a2b5d0a2d739631d19feeef0c18be4b51
Natalie Tay <natalie.tay@gmail.com>
2015-06-08 22:18:27 +0800

    Added function to check if the length of both arguments are not the same.

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -2,10 +2,16 @@ class Hamming
   VERSION = 1
 
   def self.compute(first,second)
+    checkLength(first,second)
     no_of_diff = 0
     (0..first.length).each do |i|
       no_of_diff += 1 if first[i] != second[i]
     end
     return no_of_diff
   end
+
+  def self.checkLength(first,second)
+    raise ArgumentError if first.length != second.length
+  end
+
 end
