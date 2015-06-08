6ea2ecd6b64ebb0b915f1bff818f092c6f193691
Natalie Tay <natalie.tay@gmail.com>
2015-06-08 17:38:01 +0800

    Implemented compute function for single character.

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -2,6 +2,10 @@ class Hamming
   VERSION = 1
 
   def self.compute(first,second)
-    return first.length == second.length
+    no_of_diff = 0
+    (0..first.length).each do |i|
+      no_of_diff += 1 if first[i] != second[i]
+    end
+    return no_of_diff
   end
 end
