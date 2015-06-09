f0c57e72145273beabf5e46eac78a0ed657b737a
Natalie Tay <natalie.tay@gmail.com>
2015-06-09 14:02:48 +0800

    Added 'of rna' method.

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -15,4 +15,21 @@ class Complement
     end
     return complement
   end
+
+  def self.of_rna(strand)
+    nucleotide = ''
+    strand.each_char do |char|
+      case char
+      when 'G'
+        nucleotide += 'C'
+      when 'C'
+        nucleotide += 'G'
+      when 'T'
+        nucleotide += 'A'
+      when 'A'
+        nucleotide += 'U'
+      end
+    end
+    return nucleotide
+  end
 end
