63caef566350b13c1e707abe619ba189183a95f8
Natalie Tay <natalie.tay@gmail.com>
2015-06-09 20:13:16 +0800

    Edited hamming length method name and made it private.

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -2,14 +2,16 @@ class Hamming
   VERSION = 1
 
   def self.compute(first,second)
-    check_length(first,second)
+    assert_if_same_length(first,second)
     no_of_diff = 0
     (0..first.length).each do |i|
       no_of_diff += 1 if first[i] != second[i]
     end
+    no_of_diff
   end
 
-  def self.check_length(first,second)
+  private
+  def self.assert_if_same_length(first,second)
     raise ArgumentError if first.length != second.length
   end
 
