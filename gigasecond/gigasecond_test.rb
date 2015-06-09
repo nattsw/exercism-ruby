0fb70fe92822d5dfbd038151a16401565bbedda0
Natalie Tay <natalie.tay@gmail.com>
2015-06-09 11:09:18 +0800

    Passed all tests.

--- gigasecond/gigasecond_test.rb
+++ gigasecond/gigasecond_test.rb
@@ -15,32 +15,29 @@ class GigasecondTest < Minitest::Test
   end
 
   def test_1977_06_13
-    skip
     gs = Gigasecond.from(Time.utc(1977, 6, 13, 0, 0, 0))
     assert_equal Time.utc(2009, 2, 19, 1, 46, 40), gs
   end
 
   def test_1959_07_19
-    skip
     gs = Gigasecond.from(Time.utc(1959, 7, 19, 0, 0, 0))
     assert_equal Time.utc(1991, 3, 27, 1, 46, 40), gs
   end
 
   def test_full_time_specified
-    skip
     gs = Gigasecond.from(Time.utc(2015, 1, 24, 22, 0, 0))
     assert_equal Time.utc(2046, 10, 2, 23, 46, 40), gs
   end
 
   def test_full_time_with_day_roll_over
-    skip
     gs = Gigasecond.from(Time.utc(2015, 1, 24, 23, 59, 59))
     assert_equal Time.utc(2046, 10, 3, 1, 46, 39), gs
   end
 
   # Test your 1Gs anniversary
   def test_with_your_birthday
-    skip
+    gs = Gigasecond.from(Time.utc(1989, 11, 16, 07, 25, 0))
+    assert_equal Time.utc(2021, 7, 25, 9, 11, 40), gs
   end
 
   # This test is for the sake of people providing feedback, so they
