87721b2ac5488736c01c193375312a7793e72c8b
Natalie Tay <natalie.tay@gmail.com>
2015-06-09 11:17:27 +0800

    Edited check length method name in hamming class to fit naming conventions.

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -2,7 +2,7 @@ class Hamming
   VERSION = 1
 
   def self.compute(first,second)
-    checkLength(first,second)
+    check_length(first,second)
     no_of_diff = 0
     (0..first.length).each do |i|
       no_of_diff += 1 if first[i] != second[i]
@@ -10,7 +10,7 @@ class Hamming
     return no_of_diff
   end
 
-  def self.checkLength(first,second)
+  def self.check_length(first,second)
     raise ArgumentError if first.length != second.length
   end
 
