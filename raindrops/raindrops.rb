a7047c188bc2dbe99c372c9fe886fda849b4ef15
Natalie Tay <natalie.tay@gmail.com>
2015-06-09 20:14:54 +0800

    Some refactoring for raindrops.

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,19 +1,12 @@
 class Raindrops
-  FACTORS = { 3 => "Pling", 5 => "Plang", 7 => "Plong"}
+  SPEAK = { 3 => "Pling", 5 => "Plang", 7 => "Plong"}
 
   def self.convert(input)
-    output = self.has_prime_factor(input)
-    if output == ""
-      output = input.to_s
+    rainspeak = ""
+    SPEAK.keys.each do |i|
+      rainspeak << SPEAK[i] if input.modulo(i).zero? 
     end
-    return output
-  end
 
-  def self.has_prime_factor(input)
-    output = ""
-    FACTORS.keys.each do |i|
-      output << FACTORS[i] if input.modulo(i).zero? 
-    end
-    return output
+    return rainspeak.empty? ? input.to_s : rainspeak
   end
 end
