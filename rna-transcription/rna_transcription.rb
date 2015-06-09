f0a24e31ee4399e9632b3deb19c5e58908e42847
Natalie Tay <natalie.tay@gmail.com>
2015-06-09 12:06:29 +0800

    Added logic to 'of dna' function'.

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,4 +1,15 @@
 class Complement
   def self.of_dna(strand)
+    complement = ''
+    case strand
+    when 'G'
+      complement = 'C'
+    when 'C'
+      complement = 'G'
+    when 'T'
+      complement = 'A'
+    when 'A'
+      complement = 'U'
+    end
   end
 end
