b1fae9c4aa1829e9945cc879d3a1f85c9b09a819
Natalie Tay <natalie.tay@gmail.com>
2015-06-10 12:27:13 +0800

    Added logic in difference method.

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -14,12 +14,12 @@ class Squares
   def sum_of_squares
     sum = 0
     1.upto(@value) do |i|
-      puts i**2
       sum = sum + (i**2)
     end
     sum
   end
 
   def difference
+    square_of_sums - sum_of_squares
   end
 end
